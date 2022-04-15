a66d55b838f2042683f9eca380993477594e97ac
Samuelkaa <samuelka864@gmail.com>
2022-04-15 04:03:53 +0400

    server ip change

--- Assets/Scripts/BDScripts/BDConnect.cs
+++ Assets/Scripts/BDScripts/BDConnect.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BDConnect : MonoBehaviour
 {
     public static BDConnect Instance;
-    public string serverip = "http://89.108.77.208/";
+    public string serverip = "http://localhost/";
 
     private void Awake()
     {
